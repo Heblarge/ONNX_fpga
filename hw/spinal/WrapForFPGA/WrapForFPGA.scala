3618cdd36da7379026e8e6ffca137eaf7e0f206d
zihanshuh <zihanshuh@gmail.com>
2025-12-18 08:53:12 +0000

    修改FPGA顶层slicer为slicerwrap

--- hw/spinal/WrapForFPGA/WrapForFPGA.scala
+++ hw/spinal/WrapForFPGA/WrapForFPGA.scala
@@ -101,21 +101,26 @@ case class FPGACfg(
 }
 
 case class WrapForFPGA(fpgaCfg: FPGACfg) extends Component {
-  val slicer = Slicer(fpgaCfg.slicerCfg)
-  val collector = Collector(fpgaCfg.collectorCfg)
+  val slicer = SlicerWrap(fpgaCfg.slicerCfg)
+  val collector = CollectorWrap(fpgaCfg.collectorCfg)
 
   // 创建 128 位指令的 AXI4-Lite to Stream 桥接器
   // use64BitBus = false 表示使用 32 位 AXI 总线
   val instBridge = new Inst128_Wrapper(use64BitBus = false)
-
+  def MemoryReadPortTypeA =
+    MemoryReadPort_TypeDef(AddressWidth = fpgaCfg.AddressWidth, DataWidth = fpgaCfg.systolicArraySideNum*32)
+  def MemoryReadPortTypeB =
+    MemoryReadPort_TypeDef(AddressWidth = fpgaCfg.AddressWidth, DataWidth = fpgaCfg.systolicArraySideNum*32)
+  def MemoryWritePortType =
+    MemoryWritePort_TypeDef(AddressWidth = fpgaCfg.AddressWidth, DataWidth = fpgaCfg.systolicArraySideNum*32)
   val io = new Bundle {
     // AXI4-Lite 指令接口（32 位总线）
     val sAxi4LiteInst = slave(AxiLite4(fpgaCfg.axi4LiteInstCfg.getAxiConfig))
     
     // Memory Ports - 直接暴露存储接口
-    val memPortA = master(MemoryReadPort_TypeDef(fpgaCfg.AddressWidth, fpgaCfg.dataWidth))
-    val memPortB = master(MemoryReadPort_TypeDef(fpgaCfg.AddressWidth, fpgaCfg.dataWidth))
-    val memPortZ = master(MemoryWritePort_TypeDef(fpgaCfg.AddressWidth, fpgaCfg.dataWidth))
+    val memPortA = master(MemoryReadPortTypeA)
+    val memPortB = master(MemoryReadPortTypeB)
+    val memPortZ = master(MemoryWritePortType)
     
     // 指令完成信号
     val instFinish = out Bool()
@@ -162,6 +167,7 @@ case class WrapForFPGA(fpgaCfg: FPGACfg) extends Component {
   }
 }
 
+
 object WrapForFPGA_Verilog extends App {
   val FileDir = "rtl/WrapForFPGA/verilog"
   import java.io.File
@@ -171,7 +177,7 @@ object WrapForFPGA_Verilog extends App {
     UIDWidth = 19,
     AddressWidth = 20,
     ShapeWidth = 16,
-    systolicArraySideNum = 32,
+    systolicArraySideNum = 8,
     elementWidth = 24,
     intWidth = 12,
     systolicArrayInFifoDepth = 2,
