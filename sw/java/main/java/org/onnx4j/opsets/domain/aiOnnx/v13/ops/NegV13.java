0f4860d16ab40c01a3d8f279c3853824b88a31e7
Ziyu Ma <mazy2024@shanghaitech.edu.cn>
2026-03-20 10:51:59 +0800

    修改neg算子可以识别attributes

--- sw/java/main/java/org/onnx4j/opsets/domain/aiOnnx/v13/ops/NegV13.java
+++ sw/java/main/java/org/onnx4j/opsets/domain/aiOnnx/v13/ops/NegV13.java
@@ -2,13 +2,17 @@ package org.onnx4j.opsets.domain.aiOnnx.v13.ops;
 
 import org.onnx4j.Inputs;
 import org.onnx4j.model.graph.Node;
+import org.onnx4j.model.graph.node.attributes.FloatsAttribute;
+import org.onnx4j.model.graph.node.attributes.IntsAttribute;
 import org.onnx4j.opsets.domain.aiOnnx.v13.AiOnnxOperatorV13;
 import org.onnx4j.opsets.operator.Field;
 import org.onnx4j.opsets.operator.Field.TypeConstraint;
 import org.onnx4j.opsets.operator.OperatorInputs;
+import org.onnx4j.opsets.operator.fields.AttributeField;
 import org.onnx4j.opsets.operator.fields.InputField;
 import org.onnx4j.opsets.operator.output.SingleOperatorOutputs;
 import org.onnx4j.tensor.DataType;
+import java.util.List;
 
 public interface NegV13 extends AiOnnxOperatorV13 {
 
@@ -34,15 +38,33 @@ public interface NegV13 extends AiOnnxOperatorV13 {
 
     class NegInputsV13<T_TENSOR> extends OperatorInputs<T_TENSOR> {
         protected InputField<T_TENSOR> xField;
+        public static final String ATTR_FPGA_IN_SCALES = "fpga_in_scales";
+        public static final String ATTR_FPGA_IN_SHIFT = "fpga_in_shift";
+        public static final String ATTR_FPGA_OUT_SCALES = "fpga_out_scales";
+        public static final String ATTR_FPGA_OUT_SHIFT = "fpga_out_shift";
+
+        protected Field<List<Float>> fpgaInScalesField;
+        protected Field<List<Long>> fpgaInShiftField;
+        protected Field<List<Float>> fpgaOutScalesField;
+        protected Field<List<Long>> fpgaOutShiftField;
 
         public NegInputsV13(Node node, Inputs inputs) {
             super(node, inputs);
             this.xField = new InputField<>(this, TPYE_CONSTRAINT_T, inputArray[0]);
+
+            this.fpgaInScalesField = new AttributeField<>(super.attrs, ATTR_FPGA_IN_SCALES, FloatsAttribute.class,  List.of(0.0f), true);
+            this.fpgaInShiftField = new AttributeField<>(super.attrs, ATTR_FPGA_IN_SHIFT, IntsAttribute.class,  List.of(0L), true);
+            this.fpgaOutScalesField = new AttributeField<>(super.attrs, ATTR_FPGA_OUT_SCALES, FloatsAttribute.class,  List.of(0.0f), true);
+            this.fpgaOutShiftField = new AttributeField<>(super.attrs, ATTR_FPGA_OUT_SHIFT, IntsAttribute.class,  List.of(0L), true);
         }
 
         public T_TENSOR getX() {
             return xField.getData();
         }
+        public List<Float> getFpgaInScales() { return fpgaInScalesField.getData(); }
+        public List<Long> getFpgaInShift() { return fpgaInShiftField.getData(); }
+        public List<Float> getFpgaOutScale() { return fpgaOutScalesField.getData(); }
+        public List<Long> getFpgaOutShift() { return fpgaOutShiftField.getData(); }
     }
 
     class NegOutputV13<T_TENSOR> extends SingleOperatorOutputs<T_TENSOR> {
