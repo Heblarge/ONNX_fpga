dd96f8fcf76d72dd389e46db77649a4d53a677ff
Ziyu Ma <mazy2024@shanghaitech.edu.cn>
2026-03-20 10:51:45 +0800

    修改neg算子可以识别attributes

--- sw/java/main/java/org/forwarder/backend/impls/HWAccelerated/opsets/v13/ops/HWAcceleratedNegV13.java
+++ sw/java/main/java/org/forwarder/backend/impls/HWAccelerated/opsets/v13/ops/HWAcceleratedNegV13.java
@@ -3,21 +3,38 @@ package org.forwarder.backend.impls.HWAccelerated.opsets.v13.ops;
 import org.forwarder.backend.impls.HWAccelerated.opsets.HWAcceleratedOperator;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.onnx4j.Inputs;
+import org.onnx4j.model.Graph;
 import org.onnx4j.model.graph.Node;
 import org.onnx4j.opsets.domain.aiOnnx.v13.ops.NegV13;
 import org.onnx4j.opsets.operator.OperatorOutputs;
+import org.forwarder.backend.impls.HWAccelerated.opsets.HWAcceleratedQuantizedOperator;
 
-public class HWAcceleratedNegV13 extends HWAcceleratedOperator implements NegV13 {
+
+public class HWAcceleratedNegV13 extends HWAcceleratedQuantizedOperator implements NegV13 {
 
     @Override
     public OperatorOutputs<INDArray> forward(Node node, Inputs inputs) {
         NegInputsV13<INDArray> castedInputs = new NegInputsV13<>(node, inputs);
         INDArray x = castedInputs.getX();
-        return new NegOutputV13<>(this.neg(x));
+        Graph graph = node.getGraph();
+
+        long targetInputShift = castedInputs.getFpgaInShift().get(0);
+        String inputName = node.getInputNames()[0];
+        long sourceShift = this.getProducerOutputShift(graph, inputName, targetInputShift);
+
+        long targetOutputShift = castedInputs.getFpgaOutShift().get(0);
+        return new NegOutputV13<>(this.neg(x, sourceShift, targetOutputShift));
     }
 
-    public INDArray neg(INDArray x) {
-        // 使用ND4J的neg方法计算负值
-        return x.neg();
+    public INDArray neg(INDArray x, long targetInputShift, long targetOutputShift) {
+        int rescaleAmount = (int) (targetInputShift - targetOutputShift);
+        INDArray rescaledX = (rescaleAmount == 0)
+                ? x
+                : (rescaleAmount < 0
+                ? x.mul(1L << -rescaleAmount)
+                : x.div(1L << rescaleAmount)
+        );
+
+        return rescaledX.neg();
     }
 }
